6191e6dcaa6ac07ac1d7dad8092fd5b3400f6025
KC Lim <kclim0528@gmail.com>
2024-10-22 01:12:23 -0700

    removed components into their respective files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// eslint-disable-next-line no-unused-vars
+import React, { useState , useEffect} from 'react'
 import './App.css'
+import WeatherCard from './components/WeatherCard'
+import fetchCurrentWeather from './services/fetchAPI'
+import SearchBar from './components/SearchBar'
+
+const App = ()=>{
+    
+    const [weatherData , setWeatherData] = useState(null)
+    const [error,setError] = useState(null)
+    
+    const handleSearch = async (city)=>{
+        try{
+            const data = await fetchCurrentWeather(city)
+            setWeatherData(data)
+            setError(null)
+        }catch(error){
+            setError('problem encountered while fetching weather data')
+            console.error(error,'problem at handleSearch') 
+        }
+    }
+
+    useEffect(()=>{
+        if(!import.meta.env.API_KEY){
+            console.error('API key missing')
+            setError('api key missing')
+        }
+    },[])
+    
+    return(
+        <div className="weather-app">
+            <h2>Weather App & 5 Day Forecast</h2>
+            <SearchBar onSearch={handleSearch}/>  
+            {error && <p>{error}</p>}
+            {weatherData && <WeatherCard weatherData={weatherData} />}
+                   
+        </div>
+    )
+}
 
 
+export default App
