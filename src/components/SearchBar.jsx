d54f81419920a7dd37b8914cd79741dfcb45c9cf
KC Lim <kclim0528@gmail.com>
2024-10-24 00:40:15 -0700

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,44 @@
-import {useState} from 'react'
-import PropTypes from 'prop-types'
+import { useState } from "react";
+import PropTypes from "prop-types";
+import '../styles/searchbar.css';
 
 
-const SearchBar = ({onSearch})=>{
-    const[city,setCity] = useState('')
+const SearchBar = ({ onSearch }) => {
+  const [city, setCity] = useState("");
 
-    const handleInputChange = (event)=>{
-        setCity(event.target.value)
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (city.trim() !== "") {
+      onSearch(city);
     }
+  };
+  
 
-    const searchButton = ()=>{
-        if(city.trim() !== ''){
-            onSearch(city)
-        }
-    }
-
-
-    return(
-        <div className='search-bar'>
-            <input placeholder='Type here...' type='text' value={city} onChange={handleInputChange}/>
-            <button className='btn btn-primary' onClick={searchButton}>Search</button>
+  return (
+    <div className="searchBar container my-4">
+      <form onSubmit={handleSubmit}>
+        <div className="input-group ">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter city name..."
+            value={city}
+            onChange={handleInputChange}
+          />
+          <button className="btn btn-primary searchButton" type="submit" >
+            Search
+          </button> 
         </div>
-    )
-}
+      </form>
+    </div>
+  );
+};
 SearchBar.propTypes = {
-    onSearch: PropTypes.func.isRequired,
+  onSearch: PropTypes.func.isRequired,
 };
 
-export default SearchBar
+export default SearchBar;
