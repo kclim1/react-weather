fe83d41c1f06eca6a6491b933f9a46af4cae2c2c
KC Lim <kclim0528@gmail.com>
2024-10-25 05:41:27 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,98 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState , useEffect} from 'react'
-import './styles/App.css'
-import './styles/index.css'
-import './styles/searchbar.css'
-import './styles/currentcard.css'
-import './styles/forecastcard.css'
-import {CurrentWeatherCard} from './components/CurrentWeatherCard'
-import {fetchCurrentWeather} from './services/fetchCurrentWeather'
-import { fetchForecastWeather } from './services/fetchForecastWeather'
-import SearchBar from './components/SearchBar'
-import {ForecastCard} from './components/ForecastWeatherCard'
+import React, { useState, useEffect } from "react";
+import "./styles/App.css";
+import "./styles/index.css";
+import "./styles/searchbar.css";
+import "./styles/currentcard.css";
+import "./styles/forecastcard.css";
+import { CurrentWeatherCard } from "./components/CurrentWeatherCard";
+import { fetchCurrentWeather } from "./services/fetchCurrentWeather";
+import { fetchForecastWeather } from "./services/fetchForecastWeather";
+import SearchBar from "./components/SearchBar";
+import { ForecastCard } from "./components/ForecastWeatherCard";
 // import {UnitButton} from './components/UnitButton'
 
-const App = ()=>{
-    const [forecastData,setForecastData] = useState(null)
-    const [weatherData , setWeatherData] = useState(null)
-    const [error,setError] = useState(null)
-    const [unit, setUnit] = useState('metric')
-    const [city,setCity] = useState('')
-    
+const App = () => {
+  const [forecastData, setForecastData] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+  const [unit, setUnit] = useState("metric");
+  const [city, setCity] = useState("");
 
-    const selectMetric = () => {
-        setUnit('metric');
-        console.log('metric selected')
-      };
-      
-      const selectImperial = () => {
-        setUnit('imperial');
-        console.log('imperial selected')
+  const selectMetric = () => {
+    setUnit("metric");
+    console.log("metric selected");
+  };
 
-      };
-      
-    const handleSearch = async (city)=>{
-        try{
-            setCity(city)
-            const currentData = await fetchCurrentWeather(city,unit)
-            const forecastData = await fetchForecastWeather(city,unit)
-            setForecastData(forecastData)
-            setWeatherData(currentData)
-            setError(null)
-        }catch(error){
-            setError('problem encountered while fetching weather data')
-            console.error(error,'problem at handleSearch') 
-        }
+  const selectImperial = () => {
+    setUnit("imperial");
+    console.log("imperial selected");
+  };
+
+  const handleSearch = async (city) => {
+    try {
+      setCity(city);
+      const currentData = await fetchCurrentWeather(city, unit);
+      const forecastData = await fetchForecastWeather(city, unit);
+      setForecastData(forecastData);
+      setWeatherData(currentData);
+      setError(null);
+    } catch (error) {
+      setError("problem encountered while fetching weather data");
+      console.error(error, "problem at handleSearch");
     }
-  
+  };
 
+  useEffect(() => {
+    if (!import.meta.env.VITE_OPENWEATHER_KEY) {
+      console.error("API key missing");
+      setError("API key missing");
+    }
+  }, []);
 
-    useEffect(() => {
-        if (!import.meta.env.VITE_OPENWEATHER_KEY) {
-            console.error('API key missing');
-            setError('API key missing');
-        } 
-    }, []);
+  useEffect(() => {
+    if (city) {
+      handleSearch(city);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [unit]);
 
-    useEffect(() => {
-        if (city) {
-          handleSearch(city);
-        }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [unit]);
-    
-    return (
-        <div className="weather-app">
-            <div className="currentWeather">
-            <h2>Weather App & 5 Day Forecast</h2>
-            <SearchBar onSearch={handleSearch} />
-            <div className="unit-buttons">
-  <button
-    className={`btn ${unit === 'metric' ? 'btn-primary active' : 'btn-secondary'}`}
-    onClick={selectMetric}
-  >
-    Metric
-  </button>
-  <button
-    className={`btn ${unit === 'imperial' ? 'btn-primary active' : 'btn-secondary'}`}
-    onClick={selectImperial}
-  >
-    Imperial
-  </button>
-</div>
-            {error && <p>{error}</p>}
-            {weatherData && <CurrentWeatherCard weatherData={weatherData} unit={unit} />}
-          </div>
-    
-          <div className="forecastWeather">
-            {forecastData && <ForecastCard forecastData={forecastData} unit={unit}/>}
-          </div>
+  return (
+    <div className="weather-app">
+      <div className="currentWeather">
+        <h2>Weather App & 5 Day Forecast</h2>
+        <SearchBar onSearch={handleSearch} />
+        <div className="unit-buttons">
+          <button
+            className={`btn me-2 ${
+              unit === "metric" ? "btn-primary active" : "btn-secondary"
+            }`}
+            onClick={selectMetric}
+          >
+            Metric
+          </button>
+          <button
+            className={`btn ${
+              unit === "imperial" ? "btn-primary active" : "btn-secondary"
+            }`}
+            onClick={selectImperial}
+          >
+            Imperial
+          </button>
         </div>
-      );
-    };
-    
+        {error && <p>{error}</p>}
+        {weatherData && (
+          <CurrentWeatherCard weatherData={weatherData} unit={unit} />
+        )}
+      </div>
 
-export default App
+      <div className="forecastWeather">
+        {forecastData && (
+          <ForecastCard forecastData={forecastData} unit={unit} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
